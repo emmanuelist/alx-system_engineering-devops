8c8a7f99f499ad6c0011674cdf2dfd3838c6272a
emmanuelist <emmanuel.paul75@yahoo.com>
2024-02-28 17:13:38 +0100

    Setup new Ubuntu server with Nginx and add custom HTTP header configuration

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,8 +1,9 @@
 # Setup New Ubuntu server with nginx
 # and add a custom HTTP header
 
+# Update the system
 exec { 'update system':
-        command => '/usr/bin/apt-get update',
+  command => '/usr/bin/apt-get update',
 }
 
 # Install Nginx package
@@ -10,23 +11,33 @@ package { 'nginx':
   ensure => installed,
 }
 
-# Ensure Nginx service is running and enabled
-service { 'nginx':
-  ensure  => running,
-  enable  => true,
-  require => Package['nginx'],
+# Create an index.html file
+file { '/var/www/html/index.html':
+  content => 'Hello World!',
 }
 
-# Create custom HTTP header file
+# Add a redirection rule in Nginx configuration
+exec { 'redirect_me':
+  command  => 'sed -i "24i\	rewrite ^https://www.linkedin.com/in/emmanuel-paul-391089225 permanent;" /etc/nginx/sites-available/default',
+  provider => 'shell',
+}
+
+# Add a custom HTTP header in Nginx configuration
+exec { 'HTTP header':
+  command  => 'sed -i "25i\	add_header X-Served-By \$hostname;" /etc/nginx/sites-available/default',
+  provider => 'shell',
+}
+
+# Create a separate custom header file
 file { '/etc/nginx/conf.d/custom_headers.conf':
   ensure  => present,
   content => "add_header X-Served-By $hostname;",
   notify  => Service['nginx'],
 }
 
-# Ensure custom header file is included in Nginx configuration
-file { '/etc/nginx/nginx.conf':
-  ensure  => present,
-  content => "include /etc/nginx/conf.d/*.conf;",
-  notify  => Service['nginx'],
+# Ensure Nginx service is running and enabled
+service { 'nginx':
+  ensure  => running,
+  enable  => true,
+  require => [Package['nginx'], File['/etc/nginx/conf.d/custom_headers.conf']],
 }
